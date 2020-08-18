962aca7a5c461e497d2acfe6edd3535eb01aecfb
Lingze Hu <lingzehu@gmail.com>
2020-08-17 21:58:59 -0400

    Custom Exception for Unique Project Identifier

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,14 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
+        try{
+            // Uniform the identifier to Upper case
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("ProjectId " + project.getProjectIdentifier().toUpperCase()+" already exists!");
+        }
 
-        // Logic
 
-        return projectRepository.save(project);
     }
 }
