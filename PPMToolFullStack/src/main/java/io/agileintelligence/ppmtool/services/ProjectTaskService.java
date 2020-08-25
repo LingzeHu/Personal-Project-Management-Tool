aec3c4e028fe5c80142e0c288cc4386a1207f510
Lingze Hu <lingzehu@gmail.com>
2020-08-25 18:08:22 -0400

    Persist Project Task

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,42 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Backlog;
+import io.agileintelligence.ppmtool.domain.ProjectTask;
+import io.agileintelligence.ppmtool.repository.BacklogRepository;
+import io.agileintelligence.ppmtool.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+    @Autowired
+    private BacklogRepository backlogRepository;
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        // Project Tasks(PTs) to be added to a specific project, project != null => Backlog exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        // Set the backlog to PT
+        projectTask.setBacklog(backlog);
+        // we want our project sequence to be like this: IDPRO-1
+        Integer BacklogSequence = backlog.getPTSequence();
+        // UPdate the backlog Sequence
+        BacklogSequence++;
+        // Add sequence to project task
+        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        // INITIAL priority when priority null
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+//            projectTask.setPriority(3);
+//        }
+        // INITIAL status when status null
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
